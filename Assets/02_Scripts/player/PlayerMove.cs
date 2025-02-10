738531736dc4fceceacc964cf0429280b8260dd7
dollerpeak <kobold77@empas.com>
2025-02-11 01:38:36 +0900

    타일맵 학습

--- Assets/02_Scripts/player/PlayerMove.cs
+++ Assets/02_Scripts/player/PlayerMove.cs
@@ -5,9 +5,11 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     new Rigidbody2D rigidbody2D;
     SpriteRenderer spriteRenderer;
     Animator animator;
+    RaycastHit2D raycastHit2D;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,10 +29,18 @@ public class PlayerMove : MonoBehaviour
         if (true == Input.GetKey(KeyCode.LeftArrow))
         {
             dir = -1.0f;
+            rigidbody2D.AddForce(Vector2.right * dir, ForceMode2D.Impulse);
         }
-        else if (true == Input.GetKey(KeyCode.RightArrow))
+        if (true == Input.GetKey(KeyCode.RightArrow))
         {
-            dir = 1.0f;            
+            dir = 1.0f;
+            rigidbody2D.AddForce(Vector2.right * dir, ForceMode2D.Impulse);
+        }
+        if((true == Input.GetMouseButtonDown(0) || true == Input.GetKey(KeyCode.UpArrow)) && false == animator.GetBool("isJumping"))
+        {
+            //Debug.Log("dir = " + dir);
+            rigidbody2D.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
         }
         //Debug.Log("dir = " + dir);
 
@@ -42,19 +52,22 @@ public class PlayerMove : MonoBehaviour
         }
 
         // 속도 적용
-        rigidbody2D.AddForce(Vector2.right * dir, ForceMode2D.Impulse);
+        //rigidbody2D.AddForce(Vector2.right * dir, ForceMode2D.Impulse);
         //rigidbody2D.AddForce(Vector2.right * dir);
         //rigidbody2D.linearVelocityX = dir;
 
-        // 최대 속도 제어
+        // 좌우 최대 속도 제어
         if (Mathf.Abs(rigidbody2D.linearVelocityX) >= maxSpeed)
         {
             rigidbody2D.linearVelocityX = dir * maxSpeed;
         }
 
         //
-        Debug.Log("rigidbody2D.linearVelocity.normalized.x = " + rigidbody2D.linearVelocity.normalized.x);
+        //Debug.Log("rigidbody2D.linearVelocity.normalized.x = " + rigidbody2D.linearVelocity.normalized.x);
         Debug.Log("rigidbody2D.linearVelocityX = " + rigidbody2D.linearVelocityX);
+        Debug.Log("rigidbody2D.linearVelocityY = " + rigidbody2D.linearVelocityY);
+
+        // 애니메이션
         //if (rigidbody2D.linearVelocity.normalized.x == 0.0f)
         if (Mathf.Abs(rigidbody2D.linearVelocityX) <= 1.0f)
         {
@@ -64,6 +77,22 @@ public class PlayerMove : MonoBehaviour
         {
             animator.SetBool("isWalking", true);
         }
+
+        // ray, 한번만 체크 됨
+        if (rigidbody2D.linearVelocityY <= 0.0f)
+        {
+            Debug.DrawRay(rigidbody2D.position, Vector3.down, new Color(0, 1, 0));
+            raycastHit2D = Physics2D.Raycast(rigidbody2D.position, Vector3.down, 1, LayerMask.GetMask("block"));
+            if (raycastHit2D.collider != null)
+            {
+                if (raycastHit2D.distance <= 0.5f)
+                {
+                    animator.SetBool("isJumping", false);
+                    //Debug.Log("ray name = " + raycastHit2D.collider.name);
+                }
+            }
+        }
+
 
         reStart();
     }
@@ -77,4 +106,6 @@ public class PlayerMove : MonoBehaviour
             transform.position = new Vector2(-4, 3);
         }
     }
+
+    
 }
